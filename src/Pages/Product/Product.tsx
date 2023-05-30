2134e6e50f1fb00afac202a8b1307ef07f3324d2
Pham Anh Tuan <phamanhtuan01789@gmail.com>
2023-05-30 15:02:27 +0700

    product item ui

--- src/Pages/Product/Product.tsx
+++ src/Pages/Product/Product.tsx
@@ -1,9 +1,14 @@
 import FilterBar from './FilterBar'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Images from '~/assets'
 import { Link } from 'react-router-dom'
+import { faStar } from '@fortawesome/free-regular-svg-icons'
 import styles from './Product.module.scss'
+import { useState } from 'react'
 
 function Product() {
+  const [onSale, setOnSale] = useState(true)
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
@@ -15,7 +20,10 @@ function Product() {
             <div className={styles.right_side_filter}>this is filter</div>
             <ul className={styles.right_side_products}>
               <li className={styles.right_side_item}>
-                <img src={Images.product_1} alt='item_img' />
+                {onSale && <span className={styles.item_onsale}>14% off</span>}
+                <div className={styles.item_img}>
+                  <img src={Images.product_1} alt='item_img' />
+                </div>
                 <div className={styles.item_content}>
                   <Link to='/' className={styles.item_brand}>
                     Apparel
@@ -24,10 +32,134 @@ function Product() {
                     Naturally Flavored Gold Standard 100% Whey
                   </Link>
                   <div className={styles.item_rating}>
-                    stars <span>(5)</span>
+                    <FontAwesomeIcon icon={faStar} />
+                    <FontAwesomeIcon icon={faStar} />
+                    <FontAwesomeIcon icon={faStar} />
+                    <FontAwesomeIcon icon={faStar} />
+                    <FontAwesomeIcon icon={faStar} />
+                    <span>(5)</span>
                   </div>
-                  <span className={styles.item_price}>123</span>
+                  <div className={styles.item_price}>
+                    <span className={onSale ? styles.is_sale : styles.normal_rice}>$ 123</span>
+                    {onSale && <span className={styles.sale_price}>$ 99</span>}
+                  </div>
                 </div>
+                <button className={styles.item_add_btn}>
+                  <span>add to cart</span>
+                </button>
+              </li>
+              <li className={styles.right_side_item}>
+                {onSale && <span className={styles.item_onsale}>14% off</span>}
+                <div className={styles.item_img}>
+                  <img src={Images.product_1} alt='item_img' />
+                </div>
+                <div className={styles.item_content}>
+                  <Link to='/' className={styles.item_brand}>
+                    Apparel
+                  </Link>
+                  <Link to='/' className={styles.item_name}>
+                    Naturally Flavored Gold Standard 100% Whey
+                  </Link>
+                  <div className={styles.item_rating}>
+                    <FontAwesomeIcon icon={faStar} />
+                    <FontAwesomeIcon icon={faStar} />
+                    <FontAwesomeIcon icon={faStar} />
+                    <FontAwesomeIcon icon={faStar} />
+                    <FontAwesomeIcon icon={faStar} />
+                    <span>(5)</span>
+                  </div>
+                  <div className={styles.item_price}>
+                    <span className={onSale ? styles.is_sale : styles.normal_rice}>$ 123</span>
+                    {onSale && <span className={styles.sale_price}>$ 99</span>}
+                  </div>
+                </div>
+                <button className={styles.item_add_btn}>
+                  <span>add to cart</span>
+                </button>
+              </li><li className={styles.right_side_item}>
+                {onSale && <span className={styles.item_onsale}>14% off</span>}
+                <div className={styles.item_img}>
+                  <img src={Images.product_1} alt='item_img' />
+                </div>
+                <div className={styles.item_content}>
+                  <Link to='/' className={styles.item_brand}>
+                    Apparel
+                  </Link>
+                  <Link to='/' className={styles.item_name}>
+                    Naturally Flavored Gold Standard 100% Whey
+                  </Link>
+                  <div className={styles.item_rating}>
+                    <FontAwesomeIcon icon={faStar} />
+                    <FontAwesomeIcon icon={faStar} />
+                    <FontAwesomeIcon icon={faStar} />
+                    <FontAwesomeIcon icon={faStar} />
+                    <FontAwesomeIcon icon={faStar} />
+                    <span>(5)</span>
+                  </div>
+                  <div className={styles.item_price}>
+                    <span className={onSale ?  styles.is_sale : styles.normal_rice}>$ 123</span>
+                    {onSale && <span className={styles.sale_price}>$ 99</span>}
+                  </div>
+                </div>
+                <button className={styles.item_add_btn}>
+                  <span>add to cart</span>
+                </button>
+              </li><li className={styles.right_side_item}>
+                {onSale && <span className={styles.item_onsale}>14% off</span>}
+                <div className={styles.item_img}>
+                  <img src={Images.product_1} alt='item_img' />
+                </div>
+                <div className={styles.item_content}>
+                  <Link to='/' className={styles.item_brand}>
+                    Apparel
+                  </Link>
+                  <Link to='/' className={styles.item_name}>
+                    Naturally Flavored Gold Standard 100% Whey
+                  </Link>
+                  <div className={styles.item_rating}>
+                    <FontAwesomeIcon icon={faStar} />
+                    <FontAwesomeIcon icon={faStar} />
+                    <FontAwesomeIcon icon={faStar} />
+                    <FontAwesomeIcon icon={faStar} />
+                    <FontAwesomeIcon icon={faStar} />
+                    <span>(5)</span>
+                  </div>
+                  <div className={styles.item_price}>
+                    <span className={onSale ?  styles.is_sale : styles.normal_rice}>$ 123</span>
+                    {onSale && <span className={styles.sale_price}>$ 99</span>}
+                  </div>
+                </div>
+                <button className={styles.item_add_btn}>
+                  <span>add to cart</span>
+                </button>
+              </li><li className={styles.right_side_item}>
+                {onSale && <span className={styles.item_onsale}>14% off</span>}
+                <div className={styles.item_img}>
+                  <img src={Images.product_1} alt='item_img' />
+                </div>
+                <div className={styles.item_content}>
+                  <Link to='/' className={styles.item_brand}>
+                    Apparel
+                  </Link>
+                  <Link to='/' className={styles.item_name}>
+                    Naturally Flavored Gold Standard 100% Whey
+                  </Link>
+                  <div className={styles.item_rating}>
+                    <FontAwesomeIcon icon={faStar} />
+                    <FontAwesomeIcon icon={faStar} />
+                    <FontAwesomeIcon icon={faStar} />
+                    <FontAwesomeIcon icon={faStar} />
+                    <FontAwesomeIcon icon={faStar} />
+                    <span>(5)</span>
+                  </div>
+                  <div className={styles.item_price}>
+                    <span className={onSale ?  styles.is_sale : styles.normal_rice}>$ 123</span>
+                    {onSale && <span className={styles.sale_price}>$ 99</span>}
+                  </div>
+                </div>
+                <button className={styles.item_add_btn}>
+                  <span>add to cart</span>
+                </button>
               </li>
             </ul>
           </div>
